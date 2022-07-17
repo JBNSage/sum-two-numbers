e6fe1c59f61ea4e0ad84f2877e43ee1f93ef0021
Juan Luis Bello <jlbello24@gmail.com>
2022-07-16 21:34:30 -0400

    Added project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState<number | undefined>();
+  const firstNumber = createRef<HTMLInputElement>();
+  const secondNumber = createRef<HTMLInputElement>();
+
+  const sumNumbers = () => {
+    const firstNumberValue: number = firstNumber.current?.value
+      ? parseInt(firstNumber.current?.value)
+      : 0;
+    const secondNumberValue: number = secondNumber.current?.value
+      ? parseInt(secondNumber.current?.value)
+      : 0;
+
+    const resultTMP = firstNumberValue + secondNumberValue;
+
+    setResult(resultTMP);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container p-5">
+      <h1>Suma de dos números</h1>
+      <form action="">
+        <div className="row">
+          <div className="col">
+            <input
+              onChange={sumNumbers}
+              ref={firstNumber}
+              type="number"
+              className="form-control"
+            />
+          </div>
+          <div className="col-auto">
+            <span>+</span>
+          </div>
+          <div className="col">
+            <input
+              onChange={sumNumbers}
+              ref={secondNumber}
+              type="number"
+              className="form-control"
+            />
+          </div>
+          <div className="col-auto">
+            <span> = </span>
+          </div>
+          <div className="col-auto">
+            <input
+              type="text"
+              className="form-control"
+              disabled
+              value={result}
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
